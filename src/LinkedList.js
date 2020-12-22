aea039a5a7e253911a81df07e13343e8fe5a7d82
Juraj Trappl <juro.trappl@gmail.com>
2020-12-22 15:38:19 +0100

    extracted linked list implementations

---
+++ src/LinkedList.js
@@ -0,0 +1,25 @@
+const { LinkedListNode } = require("./LinkedListNode");
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    append(value) {
+        const newNode = new LinkedListNode(value);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+
+            return this;
+        }
+
+        this.tail.next = newNode;
+        this.tail = newNode;
+
+        return this;
+    }
+}
+exports.LinkedList = LinkedList;
---
+++ src/LinkedListNode.js
@@ -0,0 +1,8 @@
+//@trekhleb
+class LinkedListNode {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+exports.LinkedListNode = LinkedListNode;
