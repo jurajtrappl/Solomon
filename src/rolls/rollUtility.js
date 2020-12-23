16afcf432e82f983975d20796f18ef4bfaa7d72f
Juraj Trappl <juro.trappl@gmail.com>
2020-12-23 13:03:44 +0100

    fixed wrong require for the mongo service

--- src/rolls/rollUtility.js
+++ src/rolls/rollUtility.js
@@ -1,3 +1,26 @@
+const { ExpressionDice } = require("./dice");
+
+const createCheckExpression = (bonus) => `1d20${(bonus > 0) ? '+' : '-'}${Math.abs(bonus)}`
+
+const prepareCheck = (bonus) => {
+    const expression = createCheckExpression(bonus);
+    
+    return {
+        dice: new ExpressionDice(expression),
+        expression: expression
+    }
+}
+
+const addBonusExpression = (currentExpr, args) => {
+    const bonusExpression = args.substring(1, args.length - 1);
+    const newExpression = currentExpr + bonusExpression;
+
+    return {
+        dice: new ExpressionDice(newExpression),
+        expression: newExpression
+    }
+}
+
 const reliableTalent = ({ visual, totalRoll }) => {
     let splitted = visual.split('+');
     let diceRoll = splitted[0].trim().substring(1, splitted[0].trim().length - 1);
@@ -16,5 +39,7 @@ const reliableTalent = ({ visual, totalRoll }) => {
 }
 
 module.exports = {
+    addBonusExpression,
+    prepareCheck,
     reliableTalent,
 }
