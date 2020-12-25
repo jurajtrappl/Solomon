2ec6f6e6a43c6b3ff50bc62c57007bf0a1f923bb
Juraj Trappl <juro.trappl@gmail.com>
2020-12-25 01:18:38 +0100

    created a new file for the new level up command

---
+++ src/commands/levelUp.js
@@ -0,0 +1,10 @@
+const { database } = require('../../settings.json'); 
+
+module.exports = {
+    name: 'levelUp',
+    description: 'Levels up a character.',
+    args: true,
+    async execute(message, args, mongo, _discordClient) {
+        //TODO
+    }
+};
