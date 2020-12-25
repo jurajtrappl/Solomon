ecf5fb3add24ee4ad8efd572b0a53c3fe7bbca5c
Juraj Trappl <juro.trappl@gmail.com>
2020-12-25 23:38:03 +0100

    replaced old loader with the new

--- command.js
+++ command.js
@@ -1,32 +1,5 @@
 const { command } = require('./settings.json');
-const { readdirSync } = require('fs');
-const { Collection } = require('discord.js');
-
-/**
- * Static class that loads comments from specified directories
- * to a discord collection
- * 
- * @class CommandLoader
- */
-class CommandLoader {
-    constructor() {
-        if (this instanceof CommandLoader) {
-            throw Error('A static class can not be instantiated.');
-        }
-    }
-
-    static load(directories) {
-        const commandCollection = new Collection();
-        for (let dir of directories) {
-            let commandFiles = readdirSync(dir).filter(file => file.endsWith('.js'));
-            commandFiles.forEach(file => {
-                const command = require(`${dir}/${file}`);
-                commandCollection.set(command.name, command);
-            })
-        }
-        return commandCollection;
-    }
-}
+const { SourceFileLoader } = require('./loader');
 
 /**
  * CommandValidator.
@@ -40,31 +13,31 @@ class CommandValidator {
 }
 
 /**
- * CommandDirector.
+ * CommandsDirector.
  * 
- * @class CommandDirector.
+ * @class CommandsDirector.
  */
-class CommandDirector {
+class CommandsDirector {
     constructor(discordClient, mongo) {
         this.discordClient = discordClient;
         this.mongo = mongo;
-        this.commands = CommandLoader.load(command.directories);
+        this.commands = SourceFileLoader.load(command.directories);
     }
 
     findCommand(commandName) {
-        return this.commands.get(commandName) || 
+        return this.commands.get(commandName) ||
             this.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
     }
 
     async execute(name, args, message) {
         const command = this.findCommand(name);
-        
+
         if (!command) return;
 
         if (command.guildOnly && message.channel.type !== 'text') {
             return await message.reply(settings.errorCommandExecuteMessage);
         }
-        
+
         try {
             await command.execute(message, args, this.mongo, this.discordClient);
         } catch (error) {
@@ -75,6 +48,6 @@ class CommandDirector {
 }
 
 module.exports = {
-    CommandDirector,
+    CommandsDirector,
     CommandValidator
 }
