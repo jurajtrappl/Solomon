001c4354d01a09963f613e9de41dbdeb57ffb8f2
Juraj Trappl <juro.trappl@gmail.com>
2020-12-29 12:56:11 +0100

    added const for for loop iterator

--- loader.js
+++ loader.js
@@ -16,7 +16,7 @@ class SourceFileLoader {
 
     static load = (directories) => {
         const sourceFileCollection = new Collection();
-        for (let dir of directories) {
+        for (const dir of directories) {
             let sourceFiles = readdirSync(dir).filter(file => file.endsWith('.js'));
             sourceFiles.forEach(file => {
                 const sourceFile = require(`${dir}/${file}`);
