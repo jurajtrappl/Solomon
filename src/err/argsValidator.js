468e72ec0dd0f8eac89b11a35ed97732260a6013
Juraj Trappl <juro.trappl@gmail.com>
2020-12-30 18:45:03 +0100

    added precheck method

--- src/err/argsValidator.js
+++ src/err/argsValidator.js
@@ -1,44 +1,71 @@
-const { ArgsCountError, InvalidArgTypeError } = require("./errors");
+const { additionalRollFlags } = require('../rolls/rollUtils');
+const { NotFoundError, searchingObjType } = require('./errors');
+const { capitalize } = require('../output/lang');
+const { database } = require('../../settings.json');
+const { isMove } = require('../combat/movement');
+const { parse } = require('../rolls/expression/parser');
+const { TileType } = require('../combat/map');
 
-const type = {
-    numeric: typeof 42,
-    string: typeof 'String'
+const isCharacterName = async (arg, mongo) => {
+    await mongo.exists(database.collections.characters, { characterName: arg });
+}
+
+const isMapObject = async (arg, mongo) => {
+    const combat = await mongo.tryFind(database.collections.data, { name: 'Combat' });
+    if (!combat) {
+        throw new NotFoundError(searchingObjType.dataFile, 'Combat');
+    }
+    const parsedMap = JSON.parse(combat.content.map);
+    return Object.keys(parsedMap.specialObjects).includes(arg);
+}
+
+const isDataFileKey = async (arg, mongo, dataFileName) => {
+    const dataFile = await mongo.tryFind(database.collections.data, { name: dataFileName });
+    if (!dataFile) {
+        throw new NotFoundError(searchingObjType.dataFile, dataFileName);
+    }
+    const skillName = capitalize(arg);
+    return Object.keys(dataFile.content).includes(skillName);
+}
+
+const timeArgs = ['m', 'h'];
+
+const typeValidators = {
+    'ability': (value, mongo) => isDataFileKey(value, mongo, 'Abilities'),
+    'characterName': isCharacterName,
+    'characterNames': (value, mongo) => value.split(',').every(elem => isCharacterName(elem, mongo)),
+    'directions': (value, _mongo) => isMove(value),
+    'mapObject': isMapObject,
+    'number': (value, _mongo) => !isNaN(value),
+    'rollExpression': (value, _mongo) => parse(value),
+    'rollFlag': (value, _mongo) => Object.values(additionalRollFlags).includes(value),
+    'skill': (value, mongo) => isDataFileKey(value, mongo, 'Skills'),
+    'string': (value, _mongo) => typeof value === typeof 'string',
+    'tileType': (value, _mongo) => Object.values(TileType).includes(value),
+    'timeArg': (value, _mongo) => timeArgs.includes(value)
 };
 
 class ArgsValidator {
-    static checkCount = (args, requiredCount) => {
-        if (args.length != requiredCount) {
-            throw new ArgsCountError(args.length, requiredCount);
-        }
-    }
+    static preCheck = async (argsSpecifics, commandArgs, mongo) => {
+        const argsOptionsResults = [];
+        for (const argsOption of argsSpecifics) {
+            //check arg count
+            if (argsOption.length != commandArgs.length && argsSpecifics.limitCount) continue;
 
-    static checkCountAtleast = (args, requiredCount) => {
-        if (args.length < requiredCount) {
-            throw new ArgsCountError(args.length, requiredCount);
-        }
-    }
-
-    static typeCheckAll = (args, expectedTypes) => {
-        //zip
-        args.map((arg, index) => {
-            this.typeCheckOne(arg, expectedTypes[index]);
-        });
-    }
-
-    static typeCheckOne = (arg, expectedType) => {
-        if (expectedType == type.numeric) {
-            if (isNaN(arg)) {
-                throw new InvalidArgTypeError(arg, expectedType);
+            let counter = 0;
+            const results = [];
+            for (const argSpec of argsOption) {
+                const argSpecResult = await typeValidators[argSpec.type](commandArgs[counter], mongo);
+                results.push(argSpecResult);
+                counter++;
             }
-        } else {
-            if (typeof arg != expectedType) {
-                throw new InvalidArgTypeError(arg, expectedType);
-            }
+            argsOptionsResults.push(results);
         }
+
+        return argsOptionsResults.length == 0 || argsOptionsResults.some(results => results.every(result => result));
     }
 }
 
 module.exports = {
-    ArgsValidator,
-    type
+    ArgsValidator
 }
